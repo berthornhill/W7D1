c51c412ed8a801b6dcf35eb43098c28f3fdf2109
berthornhill <64211468+berthornhill@users.noreply.github.com>
2020-12-07 15:29:43 -0500

    added UsersController#new form and SessionsController#new form

--- ninety_nine_cats_auth_project/db/seeds.rb
+++ ninety_nine_cats_auth_project/db/seeds.rb
@@ -6,3 +6,18 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+
+Cat.destroy_all
+
+
+Cat.create!(birth_date: "1/2/2000", color: "black", name: "Happy", sex: "M")
+Cat.create!(birth_date: "3/2/2000", color: "white", name: "Micky", sex: "M")
+Cat.create!(birth_date: "4/2/2000", color: "orange", name: "Sticky", sex: "F")
+Cat.create!(birth_date: "5/2/2000", color: "white", name: "Ricky", sex: "F")
+Cat.create!(birth_date: "10/2/2000", color: "orange", name: "ThisOldCat", sex: "F")
+Cat.create!(birth_date: "11/2/2000", color: "black", name: "Orangy", sex: "M")
+Cat.create!(birth_date: "2/6/2000", color: "brown", name: "Catunator", sex: "M")
+
+User.destroy_all
+
+User.create!(user_name: "user1", password_digest: "password1", session_token: "oljadshfaof")
