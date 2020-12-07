7bfad458bfa4042af507a3cc7198d23d363b8400
James <james.hansung.park@gmail.com>
2020-12-07 14:59:05 -0500

    added sessions controller and started user views

--- ninety_nine_cats_auth_project/app/controllers/sessions_controller.rb
+++ ninety_nine_cats_auth_project/app/controllers/sessions_controller.rb
@@ -1,15 +1,30 @@
 class SessionsController < ApplicationController
 
     def new
-
+        render :new
     end
 
     def create
+        user = User.find_by_credentials(session_params[:username], session_params[:password])
 
+        # if a user is found
+        if user
+            # log in the user with new session token
+            session[:session_token] = user.reset_session_token!
+            redirect_to cats_url
+        else
+            flash.now[:errors] = ['Invalid Credentials']
+            render :new
+        end
     end
 
     def destroy
-
+        
     end
 
+    private
+
+    def session_params
+        params.require(:user).permit(:username, :password)
+    end
 end
