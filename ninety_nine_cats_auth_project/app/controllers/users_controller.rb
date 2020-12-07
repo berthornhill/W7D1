d482c363016d679ebe1ed51101443049a5785b43
berthornhill <64211468+berthornhill@users.noreply.github.com>
2020-12-07 14:33:51 -0500

    Added user#routes, session#routes

---
+++ ninety_nine_cats_auth_project/app/controllers/users_controller.rb
@@ -0,0 +1,24 @@
+class UsersController < ApplicationController
+
+   def new
+        @users = User.new
+        render :new
+   end
+
+   def create
+        @user = User.new(user_params)
+        if @user.save
+            redirect_to user_url(@user)
+        else
+            flash.now[:errors] = @user.errors.full_messages
+            render :new
+        end
+   end
+
+
+   private
+
+   def user_params
+    params.require(:user).permit(:user_name, :password_digest, :session_token)
+   end
+end
--- ninety_nine_cats_auth_project/app/models/user.rb
+++ ninety_nine_cats_auth_project/app/models/user.rb
@@ -5,6 +5,20 @@ class User < ApplicationRecord
 
     after_initialize :set_session_token
 
+    def password=(plain_text_password)
+        @password = plain_text_password
+        self.password_digest = BCrypt::Password.create(plain_text_password)
+    end
+
+    def is_password?(plain_text_password)
+        b_crypt_password = BCrypt::Password.new(self.password_digest) 
+        b_crypt_password.is_password?(plain_text_password)
+    end
+    
+    def self.find_by_credentials(user_name, password)
+        user = User.find_by(user_name: user_name)
+    end
+
     def reset_session_token!
         session_token = SecureRandom::base64(10)
     end
